7cb0f5648319ec60709f3f614c64865d017fc7e0
blessyou <370732889@qq.com>
2020-02-19 15:06:31 +0800

    feat: 添加test

---
+++ errors_test.go
@@ -0,0 +1,40 @@
+package errors
+
+import (
+	"testing"
+)
+
+var e = HappyCommonError{
+	Code:      "500",
+	Message:   "test",
+	RequestId: "testReq",
+}
+
+func TestNewHappyCommonError(t *testing.T) {
+	r := NewHappyCommonError(e.Code, e.Message, e.RequestId)
+	str := r.Error()
+	if str != `{"code":"500","message":"test","requestId":"testReq"}` {
+		t.Error("TestNewHappyCommonError failed")
+	}
+}
+
+func TestHappyCommonError_GetCode(t *testing.T) {
+	if e.GetCode() != "500" {
+		t.Error("TestNewHappyCommonError failed")
+
+	}
+}
+
+func TestHappyCommonError_GetMessage(t *testing.T) {
+	if e.GetMessage() != "test" {
+		t.Error("TestNewHappyCommonError failed")
+
+	}
+}
+
+func TestHappyCommonError_GetRequestId(t *testing.T) {
+	if e.GetRequestId() != "testReq" {
+		t.Error("TestNewHappyCommonError failed")
+
+	}
+}
